98866527147d66533b14fc202a3ce80dc9999373
Kuli Tambang Robot <robot@smartai.alan>
2026-05-07 19:44:55 +0000

    Robot Update: Database 100 baris terbaru

---
+++ database/otakai.js
@@ -0,0 +1,122 @@
+const dataMemori = [
+  {
+    "id": 1778183098512,
+    "topic": "Berita Terkini",
+    "summary": "Eks Pegawai Ungkap Pendiri Ponpes di Pati Sering Bawa Santriwati ke Kamar",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183100621,
+    "topic": "Berita Terkini",
+    "summary": "Sultan Brunei Terbangkan Sendiri Pesawat ke Filipina untuk Hadiri KTT ASEAN",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183102642,
+    "topic": "Berita Terkini",
+    "summary": "Rekor Baru Marquinhos di Liga Champions",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183095638,
+    "topic": "Berita Terkini",
+    "summary": "Ya Ampun! Ribut dengan Tchouameni, Valverde Dilarikan ke RS",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183102364,
+    "topic": "Berita Terkini",
+    "summary": "Pemain Paling Sering Main di Liga Champions Musim Ini Adalah...",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183099537,
+    "topic": "Berita Terkini",
+    "summary": "Viral Pemain Al Khaleej 'Bantu' Al Hilal Cetak Gol, demi Jegal Ronaldo Cs?",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183101174,
+    "topic": "Berita Terkini",
+    "summary": "Pendiri Ponpes di Pati Tersangka Perkosa Santri Terancam 15 Tahun Penjara",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183101438,
+    "topic": "Berita Terkini",
+    "summary": "Waketum PSI Bro Ron soal Alasan Damai di Kasus Pemukulan: Murni Miskomunikasi",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183099286,
+    "topic": "Berita Terkini",
+    "summary": "Madrid Bisa saja Jual Vinicius atau Mbappe, Jika...",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183104214,
+    "topic": "Berita Terkini",
+    "summary": "Mauricio Souza: Peluang Persija Juara Tinggal 1 Persen",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183104157,
+    "topic": "Berita Terkini",
+    "summary": "Bikin Mikir! 9 Nama Makanan Tradisional Ini Terlalu Vulgar tapi Terkenal",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183096182,
+    "topic": "Berita Terkini",
+    "summary": "Asus Luncurkan ExpertBook Ultra, Laptop Bisnis Super Tipis Berbasis AI",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183102083,
+    "topic": "Berita Terkini",
+    "summary": "Nubia Neo 5 Series Hadir di RI, Ada Mystery Box Berisi Emas",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183097766,
+    "topic": "Berita Terkini",
+    "summary": "Kemesraan Rihanna dan A$AP Rocky di Met Gala Sebelum Terjadi 'Pertengkaran'",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183097220,
+    "topic": "Berita Terkini",
+    "summary": "Asus Pamer Ketangguhan ExpertBook Ultra, Disiram Air hingga Dibanting",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183098296,
+    "topic": "Berita Terkini",
+    "summary": "Misteri Virus Hanta di Kapal MV Hondius, Ahli: Waspada Penularan Antarmanusia",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183098351,
+    "topic": "Berita Terkini",
+    "summary": "8 Makanan Paling Bau di Dunia, Ada yang Mirip Aroma Toilet!",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183101234,
+    "topic": "Berita Terkini",
+    "summary": "Jadwal Konser Band Besar di Jakarta 2026",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183102320,
+    "topic": "Berita Terkini",
+    "summary": "Bos Xpeng Sebut 5 Produsen Mobil China Bakal Jadi Raksasa Otomotif Dunia",
+    "date": "2026-05-07"
+  },
+  {
+    "id": 1778183096476,
+    "topic": "Berita Terkini",
+    "summary": "Galaxy S26 Ultra & Buds 4 Pro Bikin Traveling Lebih Tenang",
+    "date": "2026-05-07"
+  }
+];
